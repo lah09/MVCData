0fe87cc28058502e812e49a2ad0a9e01a2de19ed
lah rufus <lahor.skolan@gmail.com>
2017-02-17 11:51:42 +0100

    some changes

---
+++ MVCData/Controllers/GuessingGameController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCData.Controllers
+{
+    public class GuessingGameController : Controller
+    {        
+        // GET: GuessingGame
+        [HttpGet]
+        public ActionResult GuessingGameForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult GuessingGameForm(int guessNum)
+        {
+            if (Session["randNum"] == null)
+            {
+                Random rand = new Random();
+                Session["randNum"] = rand.Next(1, 101);
+                Session["guessAmount"] = 0;
+            }
+            Session["guessAmount"] = (int)Session["guessAmount"] + 1;
+            Session["guess"] = guessNum;
+            int randNum = (int)Session["randNum"];
+
+            if (guessNum < randNum)
+            {
+                ViewBag.Message = "Your number is too low!";
+
+            }
+            else if (guessNum > randNum)
+            {
+                ViewBag.Message = "Your number is too high!";
+
+            }
+            else if (guessNum == randNum)
+            {
+                ViewBag.Message = "You got it!";
+            }
+            else
+            {
+                ViewBag.Message = "Ooops, wrong input!";
+            }
+
+            ViewBag.NumberOfGuess = "Number of guess: " + Session["guessAmount"];
+            return View();
+        }
+    }
+}
--- MVCData/Global.asax.cs
+++ MVCData/Global.asax.cs
@@ -9,10 +9,32 @@ namespace MVCData
 {
     public class MvcApplication : System.Web.HttpApplication
     {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                "FeverCheck",                                                   //Route name
+                "fevercheck",                                                   //URL with parameters
+                new { controller = "FeverCheck", action = "FeverCheckForm" }    // Parameter defaults
+            );
+
+            routes.MapRoute(
+                "Default",
+                "{controller}/{action}/{id}",
+                new { controller = "Home", action = "Index", id = "" }
+            );
+        }
+
         protected void Application_Start()
         {
-            AreaRegistration.RegisterAllAreas();
-            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            RegisterRoutes(RouteTable.Routes);
         }
+
+        //protected void Application_Start()
+        //{
+        //    AreaRegistration.RegisterAllAreas();
+        //    RouteConfig.RegisterRoutes(RouteTable.Routes);
+        //}
     }
 }
