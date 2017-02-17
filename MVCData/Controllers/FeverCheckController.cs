8d08d1963a8fc20403637da9ac9f36de5fa2ab13
lah rufus <lahor.skolan@gmail.com>
2017-02-17 10:30:12 +0100

    Added the fever check controller

---
+++ MVCData/Controllers/FeverCheckController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCData.Controllers
+{
+    public class FeverCheckController : Controller
+    {
+        // GET: FeverCheck      
+
+        [HttpGet]
+        public ActionResult FeverCheckForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult FeverCheckForm(float temperature)
+        {
+            if (temperature <= 36.4)
+            {
+                ViewBag.Message = "You have Hypothermia!";
+            }
+            else if (temperature >= 37.6)
+            {
+                ViewBag.Message = "You have a fever!";
+            }
+            else if (temperature >= 36.5 || temperature <= 37.5)
+            {
+                ViewBag.Message = "You're fine, nothing to worry about!";
+            }
+            else
+            {
+                ViewBag.Message = "Something went wrong!!!";
+            }
+            return View();
+        }
+    }
+}
